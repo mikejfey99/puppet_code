d85340e0f092f930dc9b5cbac5d10f6d1ff1d074
Mike Fey <michael.j.fey@microfocus.com>
2019-07-23 14:09:08 -0500

    added command help

--- Cowsay_code/init.pp
+++ Cowsay_code/init.pp
@@ -5,3 +5,12 @@ class cowsay {
   }
   include cowsay::fortune
 }
+
+
+file is in /etc/puppetlabs/code/environments/production/modules/cowsay/manafests/init.pp
+
+
+If this manifest is going to contain the cowsay class, you might be wondering why we're calling it init.pp 
+instead of cowsay.pp. Most modules contain a main class like this whose name corresponds with the name of 
+the module itself. This main class is always kept in a manifest with the special name init.pp. 
+init.pp contains the main class that matches the name of the module.
--- Cowsay_code/site.pp
+++ Cowsay_code/site.pp
@@ -1,3 +1,8 @@
 node 'cowsay.puppet.vm' {
   include cowsay
 }
+
+
+
+
+file is in /etc/puppetlabs/code/environments/production/manifests/site.pp
